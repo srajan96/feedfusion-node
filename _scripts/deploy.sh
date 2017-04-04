f567e37ece067456e33ff7304f4e7be2a5cea272
srajan96 <srajan1996@gmail.com>
2017-04-04 23:06:35 +0530

    added bg to register

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -2,9 +2,11 @@
 rm deploy.enc
 chmod 600 deploy
 ssh -i deploy deploy@139.59.64.249<<EOF
-cd feedfusion-node
+cd feedfusion
 git pull
-kilall node
-nodejs server
+rm target/hello.war
+mvn package
+rm /var/lib/tomcat8/webapps/feedfusion.war
+cp target/hello.war /var/lib/tomcat8/webapps/feedfusion.war
 echo "deployed"
 EOF
